cd8d94c74802485f5303f1b10e6d7009242874fd
dmmohr17 <dmmohr@crimson.ua.edu>
2025-10-02 16:48:59 -0500

    Update walk.py

--- walk.py
+++ walk.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from sensor_msgs.mgs import LaserScan
+from sensor_msgs.msg import LaserScan
 
 class Walk(Node):
 	def __init__(self):
