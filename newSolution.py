b933fa3f7fa152b8ef3ad79b3d51155edd81d4ba
dmmohr17 <dmmohr@crimson.ua.edu>
2025-10-08 16:49:32 -0500

    Still not working right

--- newSolution.py
+++ newSolution.py
@@ -52,10 +52,12 @@ class Walk(Node):
 				else:
 					self.move_cmd.angular.z = 2.0
 			self.move_cmd.linear.x = 0.0
-		elif(self.left < 2.0):
+		elif(self.left < 0.5 && self.direction == -1):
+			self.direction = 0.0
 			self.move_cmd.angular.z = -2.0
 			self.move_cmd.linear.x = 0.0
-		elif(self.right < 2.0):
+		elif(self.right < 0.5 && self.direction == -1)):
+			self.direction = 0.0
 			self.move_cmd.angular.z = 2.0
 			self.move_cmd.linear.x = 0.0
 		else:
