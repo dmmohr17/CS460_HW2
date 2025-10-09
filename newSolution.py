e91af919bf063e6de75dfde300427d905ca0b642
dmmohr17 <dmmohr@crimson.ua.edu>
2025-10-08 20:06:26 -0500

    Needs tuning, but some bugs fixed. Distance not great

--- newSolution.py
+++ newSolution.py
@@ -30,8 +30,8 @@ class Walk(Node):
 		middle_sensor = int(len(msg.ranges) / 2)
 		quarterAngle = int(middle_sensor / 2)
 		front = msg.ranges[middle_sensor]
-		frontLeft = msg.ranges[middle_sensor - quarterAngle]
-		frontRight = msg.ranges[middle_sensor + quarterAngle]
+		frontLeft = msg.ranges[middle_sensor + quarterAngle]
+		frontRight = msg.ranges[middle_sensor - quarterAngle]
 		#print("Sensor: " + str(front))
 		self.whisker = front
 		self.left = frontLeft
@@ -48,7 +48,7 @@ class Walk(Node):
 			if(self.cycleRotate == (1 / self.timer_period)):
 				self.cycleDetection = -1
 				self.cycleRotate = 0
-		elif(self.whisker < 0.5):
+		elif(self.whisker < 1.0):
 			if(self.left < self.right and self.direction == -1):
 				self.direction = 0
 				self.cycleDetection += 1
@@ -63,13 +63,13 @@ class Walk(Node):
 				else:
 					self.move_cmd.angular.z = 2.0
 			self.move_cmd.linear.x = 0.0
-		elif(self.left < 0.3 and self.left < self.right and (self.direction == -1 or self.direction == 0)):
+		elif(self.left < 0.5 and self.left < self.right and (self.direction == -1 or self.direction == 0)):
 			if(self.direction == -1):
 				self.cycleDetection += 1
 				self.direction = 0
 			self.move_cmd.angular.z = -1.0
 			self.move_cmd.linear.x = 0.0
-		elif(self.right < 0.3 and (self.direction == -1 or self.direction == 1)):
+		elif(self.right < 0.5 and (self.direction == -1 or self.direction == 1)):
 			if(self.direction == -1):
 				self.cycleDetection += 1
 				self.direction = 1
